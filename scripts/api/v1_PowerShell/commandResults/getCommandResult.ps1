6d320e232dbc4a6de9c72b74beb97ab4364b61a7
Scott Reed <sreed@MBP-Reed.local>
2017-09-29 11:48:20 -0600

    formatting

--- scripts/api/v1_PowerShell/commandResults/getCommandResult.ps1
+++ scripts/api/v1_PowerShell/commandResults/getCommandResult.ps1
@@ -3,24 +3,30 @@ function Get-CommandResult ()
     <#
     .SYNOPSIS
     Returns a specific command result.
+
     .DESCRIPTION
     The JSON data returned from this command stores the command output nested within the response - data - output field.
     To find the  _id of a command run the Get-CommandResults cmdlet. Note RESULT (single) vs RESULTS (mulitple).
+
     .PARAMETER JCAPIKey
     To find your JumpCLoud API key log into the JumpCloud admin portal. The API key is located from the drop down icon in the top right corner of the screen next to your admin email under 'API Settings'.
+
     .PARAMETER _id
     The command ID can be found by running thethe Get-CommandResults cmdlet. Note RESULT (single) vs RESULTS (mulitple).
+
     .EXAMPLE
-        PS> Get-commandresult | select -expandproperty response | select -expandproperty data | select -expandproperty output
-        This example retrieves the output of a Jumpcloud command
+    PS> Get-commandresult | select -expandproperty response | select -expandproperty data | select -expandproperty output
+    This example retrieves the output of a Jumpcloud command
+
     .EXAMPLE
-        PS> Get-commandresult | ConvertTo-Json
-        This example shows the all the data in JSON format returned from the cmdlet
+    PS> Get-commandresult | ConvertTo-Json
+    This example shows the all the data in JSON format returned from the cmdlet
 
     .LINK
     https://docs.jumpcloud.com/1.0/command-results/list-an-individual-command
 
     #>
+
     [CmdletBinding()]
     param
     (
--- scripts/api/v1_PowerShell/commandResults/getCommandResults.ps1
+++ scripts/api/v1_PowerShell/commandResults/getCommandResults.ps1
@@ -2,24 +2,27 @@ function Get-CommandResults ()
 {
 	<#
 	.SYNOPSIS
-		Returns up to 1000 command results.
+    Returns up to 1000 command results.
+
 	.DESCRIPTION
-		Commands are run from the JumpCloud admin portal. NOTE that the output of the command is not displayed in the command results. To find this you must run Get-Commandresult which returns the output for a single command using the _id. Use Get-commandresults to find the _id of commands.
+    Commands are run from the JumpCloud admin portal. NOTE that the output of the command is not displayed in the command results. To find this you must run Get-Commandresult which returns the output for a single command using the _id. Use Get-commandresults to find the _id of commands.
 
     .PARAMETER JCAPIKey
-        To find your JumpCLoud API key log into the JumpCloud admin portal. The API key is located from the drop down icon in the top right corner of the screen next to your admin email under 'API Settings'. 
+    To find your JumpCLoud API key log into the JumpCloud admin portal. The API key is located from the drop down icon in the top right corner of the screen next to your admin email under 'API Settings'.
 
 	.EXAMPLE
-		PS> Get-CommandResults | Sort-Object -property requestTime -Descending | select -last 5
-        This example retrieves the last five commands run sorted by requestTime
+	PS> Get-CommandResults | Sort-Object -property requestTime -Descending | select -last 5
+    This example retrieves the last five commands run sorted by requestTime
 
     .EXAMPLE
-		PS> Get-commandresults | Select _ID, Name
-		This example retrieves the _ID and Name for up to 1000 command results
-	.LINK
-		https://docs.jumpcloud.com/1.0/command-results/list-all-command-results
+	PS> Get-commandresults | Select _ID, Name
+    This example retrieves the _ID and Name for up to 1000 command results
 
-	#>
+	.LINK
+    https://docs.jumpcloud.com/1.0/command-results/list-all-command-results
+
+    #>
+
     [CmdletBinding()]
     param
     (
