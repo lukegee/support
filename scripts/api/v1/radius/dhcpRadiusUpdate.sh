ac1a2f0e02fe9a7115d641e7c82889f171db9bff
Luke Gripenberg <lukegee@users.noreply.github.com>
2017-11-21 20:19:22 +0100

    Update dhcpRadiusUpdate.sh
    Removed requirement for root user, lowest privilege is safest

--- scripts/api/v1/radius/dhcpRadiusUpdate.sh
+++ scripts/api/v1/radius/dhcpRadiusUpdate.sh
@@ -19,11 +19,6 @@
 #
 ###############################################################################
 
-# Got root?
-
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" && exit 1
-fi
 
 # Define Vars
 apiKey=
